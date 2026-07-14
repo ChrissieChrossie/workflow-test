8c8a1c3aed4ca52c2be17de54bfcc7e570315040
Christopher <CWolter1@gmx.de>
2026-07-14 20:06:52 +0200

    Add security group in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,10 +3,11 @@ provider "aws" {
 }
 
 resource "aws_instance" "erste_instanz" {
-  count         = var.instance_count
-  ami           = "ami-0303e2e4a29f041a3"
-  instance_type = var.aws_instance_type
-  key_name      = "Toffee - Workflow - Key"
+  count                  = var.instance_count
+  ami                    = "ami-0303e2e4a29f041a3"
+  instance_type          = var.aws_instance_type
+  key_name               = "Toffee - Workflow - Key"
+  vpc_security_group_ids = ["sg-08812866a8273da82"]
   tags = {
     Name = "Mein Webserver"
   }
